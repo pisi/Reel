418496255e5f894f045f45a493d902c0fb33ca3a
Petr Vostrel <petr@vostrel.cz>
2010-05-20 17:30:13 +0200

    Internal ticker tempo tests added
    So far 4 tests are failing

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,33 @@
 /**
  * .reel Unit Test
  */
-(function($){
+(function($, document){
 
 // All tests reside inside `unit/` folder
 
-})(jQuery);
+	module('Animation', { teardown: function teardown(){
+	  $('.jquery-reel').trigger('teardown');
+	} });
+
+	asyncTest( 'Very first initiated Reel sets `tempo` of ticker shared by all others', function(){
+		var
+			fps= 6,
+			$reel= $('#image').reel({ tempo: fps }),
+			should_be= 1000 / fps, // 6 fps ~ 166.67ms tick
+			ticks= [],
+			timeout,
+			$pool= $(document).bind('tick.reel', tick);
+		function tick(){
+			if (ticks.length < 2){
+				ticks.push(new Date());
+			}else if (ticks.length == 2){
+				timeout= ticks[1] - ticks[0];
+				ok( timeout > should_be-1 && timeout < should_be+1,
+					'Measured delay between two ticks matches tempo given in options' );
+				$pool.unbind('tick.reel', tick);
+				start();
+			}
+		}
+	});
+
+})(jQuery, this);
