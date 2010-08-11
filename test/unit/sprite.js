d65d4270ac2f4c914db40980755214c6019ee3eb
Petr Vostrel <petr@vostrel.cz>
2010-08-11 12:11:01 +0200

    Erased redundant comment

--- test/unit/sprite.js
+++ test/unit/sprite.js
@@ -8,16 +8,6 @@
   }});
 
   asyncTest( 'Multi-row: Frame shifting (4 rows)', function(){
-  /*
-  A1 A2 A3
-  A4 A5 A6
-  B1 B2 B3
-  B4 B5 B6
-  C1 C2 C3
-  C4 C5 C6
-  D1 D2 D3
-  D4 D5 D6
-  */
 		expect(12);
     var
       selector= '#image',
