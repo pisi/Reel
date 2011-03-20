86fb01305999f72ea4388e27e852259ff70ac3f9
Petr Vostrel <petr@vostrel.cz>
2011-03-20 16:02:44 +0100

    Improved average tick duration measurement and returned back to lower 10% tolerance.

--- test/tests.js
+++ test/tests.js
@@ -12,20 +12,28 @@
 	asyncTest( 'Very first initiated Reel sets `tempo` of ticker shared by all others', function(){
 		expect(1);
 		var
-			fps= 3,
+			fps= 36,
 			lazy= (/iphone|ipod|android/i).test(navigator.userAgent),
 			$reel= $('#image').reel({ tempo: fps }),
-			should_be= 1000 / fps * (lazy? $.reel.laziness:1),
+			should_be= 1000 / fps * (lazy? $.reel.def.laziness:1),
 			ticks= [],
-			timeout,
+			samples= 72,
+			sum= 0,
+			duration,
 			$pool= $(document).bind('tick.reel', tick);
 		function tick(){
-			if (ticks.length < 2){
+			if (ticks.length <= samples){
+				// Collect time signatures until `samples` target reached
 				ticks.push(new Date());
-			}else if (ticks.length == 2){
-				timeout= ticks[1] - ticks[0];
-				ok( timeout > should_be - 0.2*should_be && timeout < should_be + 0.2*should_be,
-					'Measured delay between two ticks matches tempo given in options (± 20 %)' );
+			}else if (ticks.length > samples){
+				while (ticks.length > 1){
+					// Cut off last signature and substract the preceding one to get the time difference
+					sum+= ticks.pop() - ticks[ticks.length - 1]
+				}
+				// Calculate average tick duration
+				duration= sum / samples;
+				ok( duration > should_be - 0.1*should_be && duration < should_be + 0.1*should_be,
+					'Measured delay between two ticks matches tempo given in options (± 10 %)' );
 				$pool.unbind('tick.reel', tick);
 				start();
 			}
--- test/unit/animation.js
+++ test/unit/animation.js
@@ -10,22 +10,28 @@
 	asyncTest( 'Speed of ticker can not be changed by any consequent Reel', function(){
 		expect(1);
 		var
-			fps= 3,
+			fps= 36,
 			lazy= (/iphone|ipod|android/i).test(navigator.userAgent),
 			$reel= $('#image').reel({ tempo: 25 }),
-			should_be= 1000 / fps * (lazy? $.reel.laziness:1),
+			should_be= 1000 / fps * (lazy? $.reel.def.laziness:1),
 			ticks= [],
-			start= new Date(),
-			timeout= 0,
+			samples= 72,
+			sum= 0,
+			duration,
 			$pool= $(document).bind('tick.reel', tick);
 		function tick(){
-			if (ticks.length < $.reel.def.tempo){
+			if (ticks.length <= samples){
+				// Collect time signatures until `samples` target reached
 				ticks.push(new Date());
-			}else if (ticks.length == $.reel.def.tempo){
-				ticks.push(new Date());
-        timeout= ticks[ticks.length - 1] - ticks[0];
-				ok( timeout < should_be + 0.2*should_be && timeout > should_be - 0.2*should_be,
-					'Measured delay between two ticks matches given 6fps tempo despite setting it to 25fps (± 20 %)' );
+			}else if (ticks.length > samples){
+				while (ticks.length > 1){
+					// Cut off last signature and substract the preceding one to get the time difference
+					sum+= ticks.pop() - ticks[ticks.length - 1]
+				}
+				// Calculate average tick duration
+				duration= sum / samples;
+				ok( duration < should_be + 0.1*should_be && duration > should_be - 0.1*should_be,
+					'Measured delay between two ticks matches given 36fps tempo despite setting it to 25fps (± 10 %)' );
 				$pool.unbind('tick.reel', tick);
 				start();
 			}
