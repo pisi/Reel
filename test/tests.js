39b576e8253fa801fdbc5504536e1012a89b5a15
Petr Vostřel <petr@vostrel.cz>
2011-10-01 16:56:26 +0200

    Added an explainer for the test results collection.

--- test/tests.js
+++ test/tests.js
@@ -9,6 +9,19 @@
       $('body').addClass('failure');
     }
 
+    /*
+     * Results of the just finished testrun are automatically submitted
+     * and collected on the Reel webserver as a kind of distributed
+     * testing effort. Thanks for being a part of it!
+     *
+     * Data collected:
+     * - timestamp
+     * - counts (total number of tests, fails, passes)
+     * - failures themselves
+     * - versions (version of jQuery and Reel)
+     * - the results summary line with duration
+     * - user agent string (your browser name and its version(s))
+     */
     $.post('http://jquery.vostrel.cz/collect/reel/testrun/results', {
       timestamp:  +new Date(),
       filter:     config.filters,
