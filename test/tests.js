b575e4420325e6bc088d7aa1643784c026932af2
Petr Vostřel <petr@vostrel.cz>
2011-10-01 16:50:33 +0200

    Added a POST call to the Reel server to collect the currently finished testrun results. Data collected: - time - counts (total number of tests, fails, passes) - failures themselves - versions (version of jQuery and Reel) - the results summary line with duration - user agent string

--- test/tests.js
+++ test/tests.js
@@ -8,7 +8,31 @@
     if (failures){
       $('body').addClass('failure');
     }
-  }
 
+    $.post('http://jquery.vostrel.cz/collect/reel/testrun/results', {
+      timestamp:  +new Date(),
+      filter:     config.filters,
+      count: {
+        total:    total,
+        pass:     total - failures,
+        fail:     failures
+      },
+      fails:      dump($('#qunit-tests li.fail')),
+      version: {
+        jquery:   $().jquery,
+        reel:     $.reel.version
+      },
+      results:    $('#qunit-testresult').html(),
+      agent:      $('#qunit-userAgent').html()
+    });
+
+    function dump($collection){
+      var collection = [];
+      $collection.each(function(){
+        collection.push($(this).html())
+      })
+      return collection;
+    }
+  }
 
 })(jQuery);
