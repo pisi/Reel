07157c03432ec97bdee7a82601825b1e1f89126e
Petr Vostřel <petr@vostrel.cz>
2012-01-17 20:36:44 +0100

    Fixed the host offset calculation to not take place when no original event has been handed over to it.

--- jquery.reel.js
+++ jquery.reel.js
@@ -438,7 +438,7 @@ var
                 slidable= false;
                 unidle();
                 var
-                  host_offset= !$(ev.currentTarget).is(pool) ? $iframe.offset() : { left: 0, top: 0 },
+                  host_offset= ev && !$(ev.currentTarget).is(pool) ? $iframe.offset() : { left: 0, top: 0 },
                   y= y - host_offset.top,
                   x= x - host_offset.left,
                   delta= { x: x - last.x, y: y - last.y }
