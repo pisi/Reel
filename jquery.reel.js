70d288d721927ecd95d793cb549c5077027fae12
Petr Vostřel <petr@vostrel.cz>
2012-05-21 18:22:08 +0200

    Removed the legacy of old `click` binding

--- jquery.reel.js
+++ jquery.reel.js
@@ -1473,7 +1473,7 @@ jQuery.reel || (function($, window, document, undefined){
                   // This behavior can be disabled by the [`steppable`](#steppable-Option) option.
                   //
                   'up.steppable': function(e, ev){
-                    if (panned || wheeled) return mute(e, false);
+                    if (panned || wheeled) return;
                     t.trigger(get(_clicked_location_).x - t.offset().left > 0.5 * get(_dimensions_).x ? 'stepRight' : 'stepLeft')
                   },
                   'stepLeft stepRight': function(e){
