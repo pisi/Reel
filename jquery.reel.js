16eb1e07dc381af496aa9d51b220e9c7208fa334
Petr Vostřel <petr@vostrel.cz>
2013-08-31 02:31:14 +0200

    `shy` option documentation

--- jquery.reel.js
+++ jquery.reel.js
@@ -494,6 +494,13 @@
         //
         preload:       'fidelity',
 
+        // Sometimes, on-demand activation is desirable in order to conserve device resources or bandwidth
+        // especially with multiple instances on a single page. If so, enable _shy mode_, in which Reel will
+        // hold up the initialization process until the image is clicked by the user. Alternativelly you can
+        // initialize shy instance by triggering `"setup"` event.
+        //
+        // ---
+
         // #### `shy` Option ####
         // [NEW] `Boolean`, since 1.3
         //
